b2910d95f7ba1f831dd6f942ed785c5dee09a77c
yoyomi-T <calidoscopio12070710@gmail.com>
2025-07-15 00:55:49 +0900

    Delete molecure_game directory

--- molecure_game/data/monomers.js
+++
@@ -1,9 +0,0 @@
-// data/monomers.js
-const allMonomers = [
-    { id: 'glucose', name: 'グルコース', image: 'images/monomers/glucose.png', type: 'monosaccharide' },
-    { id: 'fructose', name: 'フルクトース', image: 'images/monomers/fructose.png', type: 'monosaccharide' },
-    { id: 'alanine', name: 'アラニン', image: 'images/monomers/amino_acid_alanine.png', type: 'amino_acid' },
-    { id: 'glycine', name: 'グリシン', image: 'images/monomers/amino_acid_glycine.png', type: 'amino_acid' },
-    // ...他のモノマー
-];
-export { allMonomers };
